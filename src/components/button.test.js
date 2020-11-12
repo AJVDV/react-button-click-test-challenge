ec7011e87dc71ac8824ceeaa073fab71246aff16
Andrew VanDerVeen <69212799+AJVDV@users.noreply.github.com>
2020-11-12 14:26:53 -0800

    Update button.test.js

--- src/components/button.test.js
+++ src/components/button.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import Button from './button';
-
+ 
 describe('<Button />', () => {
     it('Should call props.onClick when the button is clicked', () => {
         const wrapper = shallow(<Button />);
